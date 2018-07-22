db6e04bfdf026deeb64814b87fa13fc43201a1be
Can Hanhan <p_github@finarfin.net>
2018-07-22 14:17:09 -0500

    Build documentation

--- src/files/docs.tasks.ps1
+++ src/files/docs.tasks.ps1
@@ -7,7 +7,6 @@ task GenerateDocs -precondition { -not $IsScript } -depends Stage -requiredVaria
 
         $module = Import-Module -Name $filePath -Global -Force -PassThru
 
-        Write-Host (Get-Module | Format-List | Out-String)
         if (-not (Test-Path -Path $docPath)) { New-Item -Path $docPath -ItemType Directory -Force | Out-Null }
 
         New-MarkdownHelp -Module $name -OutputFolder $docPath -WithModulePage -Force | Out-Null
@@ -17,6 +16,10 @@ task GenerateDocs -precondition { -not $IsScript } -depends Stage -requiredVaria
             $doc = Join-Path -Path $docPath -ChildPath "$($function).md"
             Update-MarkdownHelp -Path $doc | Out-Null
         }
-
     } | Receive-Job -Wait -AutoRemoveJob
+}
+
+task "BuildDocs" -depends "GenerateDocs" -requiredVariables "BuildOutput", "DocumentationPath" {
+    $destination = Join-Path -Path $BuildOutput -ChildPath "en-US"
+    New-ExternalHelp -Path $DocumentationPath -OutputPath $destination | Out-Null
 }
