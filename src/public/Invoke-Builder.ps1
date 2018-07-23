f54d96b57be0be36d39865749a58c0682e5168d9
Can Hanhan <p_github@finarfin.net>
2018-07-22 22:36:26 -0500

    Removed configuration logic

--- src/public/Invoke-Builder.ps1
+++ src/public/Invoke-Builder.ps1
@@ -5,44 +5,17 @@ function Invoke-Builder
         [Parameter(Position=0, ValueFromRemainingArguments=$true)]
         [string[]]$Tasks = (, "default"),
 
-        [Parameter(Mandatory=$true, ParameterSetName="UseFile")]
-        [ValidateNotNullOrEmpty()]
-        [string]$ConfigurationFile="build.json",
-
-        [Parameter(Mandatory=$true, ParameterSetName="UseHashtable")]
-        [ValidateNotNull()]
-        [hashtable]$Configuration=@{},
-
         [Parameter(Mandatory=$false)]
         [ValidateNotNullOrEmpty()]
         [string[]]$TestTags,
 
-        [switch]$ExitWithCode
+        [switch]$ExitOnError
     )
 
     $buildRoot = (Get-Location).Path
     $buildFile = "$PSScriptRoot/files/build.tasks.ps1"
 
-    if (-not [IO.Path]::IsPathRooted($ConfigurationFile))
-    {
-       $ConfigurationFile = Join-Path -Path $buildRoot -ChildPath $ConfigurationFile
-    }
-
-    if (-not (Test-Path -Path $ConfigurationFile))
-    {
-        if ($PSCmdlet.ParameterSetName -eq "UseFile") { throw "Configuration file does not exist in $ConfigurationFile" }
-    }
-    else
-    {
-        $config = Get-Content $ConfigurationFile | ConvertFrom-Json
-        @(Get-Member -InputObject $config -MemberType NoteProperty).ForEach({
-            $Configuration[$_.Name] = $config."$($_.Name)"
-        })
-    }
-
-    $Configuration["BuildRoot"] = $buildRoot
-
-    Invoke-psake -buildFile $BuildFile -nologo -parameters $Configuration -taskList $Tasks
+    Invoke-psake -buildFile $BuildFile -nologo -parameters @{ "BuildRoot" = $buildRoot } -taskList $Tasks
 
     if (-not $psake.build_success)
     {
--- tests/Invoke-Builder.tests.ps1
+++ tests/Invoke-Builder.tests.ps1
@@ -1,41 +1,5 @@
 Describe "Invoke-Builder" {
     InModuleScope -ModuleName "PSBuilder" {
-        Context "PSake successful" {
-            Mock -CommandName "Invoke-PSake" -MockWith { $Global:psake = @{ build_success = $true } }
-
-            It "throws if configuration file is specified but missing" {
-                { Invoke-Builder -ConfigurationFile "$TestDrive/NotExistingPath" SampleTask } | Should -Throw
-            }
-
-            It "does not throw when configuration file is not specified" {
-                Mock -CommandName "Test-Path" -MockWith { $False }
-                Invoke-Builder SampleTask
-            }
-
-            It "handles relative config file paths" {
-                $CurrentPath = Join-Path -Path $PSScriptroot -ChildPath "sample.json"
-
-                Mock -CommandName "Get-Content" -MockWith { "{}" }
-                Mock -CommandName "Test-Path" -MockWith { $True } -Verifiable -ParameterFilter { $Path -eq $CurrentPath }
-
-                Invoke-Builder -ConfigurationFile "sample.json"
-
-                Assert-VerifiableMock
-            }
-
-            It "reads the properties from build file" {
-                $Global:Configuration = $null
-                Mock -CommandName "Get-Content" -MockWith { "{ 'sample_key': 'sample_value' }" }
-                Mock -CommandName "Test-Path" -MockWith { $True }
-                Mock -CommandName "Invoke-psake" -MockWith { $Global:Configuration = $Parameters }
-
-                Invoke-Builder SampleTask
-
-                $Configuration.Keys | Should -Contain "sample_key"
-                $Configuration["sample_key"] | Should -Be "sample_value"
-            }
-        }
-
         Context "PSake failed" {
             Mock -CommandName "Invoke-psake" -MockWith { $Global:psake = @{ build_success = $false } }
 
@@ -47,7 +11,7 @@ Describe "Invoke-Builder" {
                 Mock -CommandName "Exit-Powershell" -MockWith { $Global:ExitCode = $ExitCode } -Verifiable
 
                 It "exits on error" {
-                    Invoke-Builder SampleTask -ExitWithCode
+                    Invoke-Builder SampleTask -ExitOnError
 
                     Assert-VerifiableMock
                 }
