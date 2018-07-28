e0fc1255c59d8c5119f8fa4116f2457e618b3d14
Can Hanhan <p_github@finarfin.net>
2018-07-28 12:16:13 -0500

    Version bump

--- build.ps1
+++ build.ps1
@@ -2,6 +2,6 @@ $Name = 'PSBuilder'
 $Guid = '4e489c66-8a1a-11e8-9a94-a6cf71072f73'
 $Author = 'Can Hanhan'
 $Description = 'PSBuilder provides re-usable, shared build tasks for building Powershell modules and scripts.'
-$Version = '0.0.1'
+$Version = '0.0.2'
 $PublishRepository = "PSGallery"
 $CodeCoverageMin = 60
